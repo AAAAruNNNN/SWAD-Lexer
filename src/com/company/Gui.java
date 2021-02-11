9ca30560da224d77f475e1aee2d264f6b464ad4a
chandubuoy <csb1619@gmail.com>
2021-02-10 19:49:55 -0700

    made function changes to GUI class

--- src/com/company/Gui.java
+++ src/com/company/Gui.java
@@ -19,25 +19,30 @@ import java.util.Vector;
 
 public class Gui extends JFrame {
     private JFrame mainFrame;
+    private JPanel middlePanel;
     private JTextArea editor;
     private JTable tokensTable;
     private JTextArea console;
     private DefaultTableModel tableModel;
 
-    private static final String FRAMETITLE = "LEXICAL ANALYZER ";
-    private static final int FRAMEWIDTH = 500;
-    private static final int FRAMEHEIGHT = 500;
-    private static final String FILEMENULABEL = "File";
-    private static final String RUNMENULABEL = "Run";
-    private static final String[] columnNames = { "Line", "Token", "String or word" };
+    private static final String FRAME_TITLE = "LEXICAL ANALYZER ";
+    private static final int FRAME_WIDTH = 500;
+    private static final int FRAME_HEIGHT = 500;
+    private static final String FILE_MENU_LABEL = "File";
+    private static final String RUN_MENU_LABEL = "Run";
+    private static final String[] COLUMN_NAMES = {"Line", "Token", "String or word"};
+    private static final String EDITOR_PANEL_TITLE = "Source Code";
+    private static final String TOKEN_TABLE_TITLE = "Token Table";
+    private static final String CONSOLE_PANEL_TITLE = "Console";
+    private static final String ERROR = "ERROR";
 
     public Gui() {
         createGui();
     }
 
     private void createGui() {
-        mainFrame = new JFrame(FRAMETITLE);
-        mainFrame.setSize(FRAMEWIDTH, FRAMEHEIGHT);
+        mainFrame = new JFrame(FRAME_TITLE);
+        mainFrame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         createMenu();
         createMiddlePanel();
         createConsole();
@@ -47,19 +52,17 @@ public class Gui extends JFrame {
 
     private void createMenu() {
         JMenuBar menuBar = new JMenuBar();
-        JMenu fileMenu = new JMenu(FILEMENULABEL);
-        Action runAction = new AbstractAction(RUNMENULABEL) {
+        JMenu fileMenu = new JMenu(FILE_MENU_LABEL);
+        Action runAction = new AbstractAction(RUN_MENU_LABEL) {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String s = editor.getText();
-                Lexer lexer = new Lexer(s);
+                Lexer lexer = new Lexer(editor.getText());
                 lexer.run();
                 Vector<Token> tokens = lexer.getTokens();
-                writeLexicalAnalysis(tokens);
+                writeLexicalAnalysisResults(tokens);
                 writeConsole(tokens);
             }
         };
-
         JToggleButton runMenu = new JToggleButton(runAction);
         menuBar.add(fileMenu);
         menuBar.add(runMenu);
@@ -67,34 +70,33 @@ public class Gui extends JFrame {
         mainFrame.setJMenuBar(menuBar);
     }
 
-
-
-
     private void createMiddlePanel() {
-        JPanel middlePanel = new JPanel();
+        middlePanel = new JPanel();
         middlePanel.setLayout(new GridLayout(1, 2));
         createEditor();
-        JScrollPane editorScrollPane = new JScrollPane(editor);
-        editorScrollPane.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.BLACK), "Source Code"));
-        middlePanel.add(editorScrollPane);
         createTokensTable();
-        JScrollPane tableScrollPane = new JScrollPane(tokensTable);
-        tableScrollPane.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.BLACK), "Token Table"));
-        middlePanel.add(tableScrollPane);
         mainFrame.add(middlePanel);
     }
 
     private void createEditor() {
         editor = new JTextArea();
         editor.setLineWrap(true);
+        JScrollPane editorScrollPane = new JScrollPane(editor);
+        editorScrollPane.setBorder(
+                BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.BLACK), EDITOR_PANEL_TITLE));
+        middlePanel.add(editorScrollPane);
     }
 
     private void createTokensTable() {
-        tableModel = new DefaultTableModel(columnNames, 0);
+        tableModel = new DefaultTableModel(COLUMN_NAMES, 0);
         tokensTable = new JTable(tableModel);
+        JScrollPane tableScrollPane = new JScrollPane(tokensTable);
+        tableScrollPane.setBorder(
+                BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.BLACK), TOKEN_TABLE_TITLE));
+        middlePanel.add(tableScrollPane);
     }
 
-    private void writeLexicalAnalysis(Vector<Token> tokens) {
+    private void writeLexicalAnalysisResults(Vector<Token> tokens) {
         writeTokenTable(tokens);
         writeConsole(tokens);
     }
@@ -102,14 +104,14 @@ public class Gui extends JFrame {
     private void writeTokenTable(Vector<Token> tokens) {
         Object[] tokenData;
         clearTokenTable(tableModel);
-        for(Token token: tokens) {
+        for (Token token : tokens) {
             tokenData = new Object[]{token.getLine(), token.getToken(), token.getWord()};
             tableModel.addRow(tokenData);
         }
     }
 
     private void clearTokenTable(DefaultTableModel tableModel) {
-        if(tableModel.getRowCount()>0) {
+        if (tableModel.getRowCount() > 0) {
             for (int i = tableModel.getRowCount() - 1; i > -1; i--) {
                 tableModel.removeRow(i);
             }
@@ -118,7 +120,8 @@ public class Gui extends JFrame {
 
     private void createConsole() {
         console = new JTextArea();
-        console.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.BLACK), "Console"));
+        console.setBorder(BorderFactory.createTitledBorder(
+                BorderFactory.createLineBorder(Color.BLACK), CONSOLE_PANEL_TITLE));
         console.setLineWrap(true);
         console.setBackground(Color.gray);
         mainFrame.add(console);
@@ -126,27 +129,26 @@ public class Gui extends JFrame {
 
     private void writeConsole(Vector<Token> tokens) {
         int numberOfStrings = tokens.size();
-        if(numberOfStrings>0){
+        if (numberOfStrings > 0) {
             int numberOfLines = tokens.lastElement().getLine();
             int numberOfErrors = getNumberOfErrors(tokens);
             String plural;
-            if(numberOfErrors==1) {
+            if (numberOfErrors == 1) {
                 plural = " does";
             } else {
                 plural = "s do";
             }
-            console.setText(numberOfStrings+" strings found in "+numberOfLines+" lines.\n"+
-                    numberOfErrors+" string"+plural+" not match any rule");
+            console.setText(numberOfStrings + " strings found in " + numberOfLines + " lines.\n" +
+                    numberOfErrors + " string" + plural + " not match any rule");
         } else {
             console.setText("No input in editor");
         }
-
     }
 
     private int getNumberOfErrors(Vector<Token> tokens) {
         int numberOfErrors = 0;
-        for(Token token : tokens) {
-            if(token.getToken().equals("ERROR")) {
+        for (Token token : tokens) {
+            if (token.getToken().equals(ERROR)) {
                 numberOfErrors++;
             }
         }
